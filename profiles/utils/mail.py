acf2e520aa370bbd4c335f2363a20d5c2e7d1dbc
swss0 <swss0011@gmail.com>
2023-02-13 20:22:11 +0200

    formatted html verification email. fixed!

--- profiles/utils/mail.py
+++ profiles/utils/mail.py
@@ -426,21 +426,6 @@ async def send_email(email: str, linkTo: str, is_verification: bool = True):
             </html>
         """
 
-        template = f"""
-                        <html>
-                            <head>
-                            </head>
-                            <body>
-                                <div style = "display: flex; align-items: center; justify-content: center; flex-direction: column">
-                                    <h3>Account Verification</h3>
-                                    <br>
-                                    <p>Dear Customer, Please open the link:</p>
-                                    <a stle="margin-top: 1rem; padding: 1rem; border-radius: 0.5rem; font-size: 1rem; text-decoration: none; background: #275d8; color: white;" href="{temp_url}{link_to}">{msg_text}</a>
-                                    <p>Please kindly ignore this email if you did not register for our service. Thanks</p>
-                                </div>
-                            </body>
-                        </html>
-                    """
     else:
         temp_url = verify_url
         msg_text = "In Order To Change Your Password"
